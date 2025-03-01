64b3f8c258ef6cc33de9e22f2b11f298e76c1e74
Franck <francklopez92@outlook.es>
2025-03-01 10:51:11 +0100

    Update Vehiculo
    Creacion Clase Vehiculo

--- src/Transporte/Vehiculo.java
+++ src/Transporte/Vehiculo.java
@@ -1,3 +1,25 @@
-public class Vehiculo {
+public abstract class Vehiculo {
+    //Atributo de la clase vehiculo protected porque sera heredado
+    protected int velocidadMaxima;
+
+    //Constructor de la clase vehiculo
+    public Vehiculo(int velocidadMaxima) {
+        this.velocidadMaxima = velocidadMaxima;
+    }
+
+    //Metodo get de la clase vehiculo
+    public int getVelocidadMaxima() {
+        return velocidadMaxima;
+    }
+
+    //Metodo arrancar() de la clase vehiculo
+    public void arrancar(){
+        System.out.println("El vehiculo esta en marcha");
+    }
+
+    //Metodo abstracto de la clase vehiculo
+    public abstract void describir();
+
+
 
 }
