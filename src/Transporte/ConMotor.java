816d13b4daeb54ecd0288c9413fe75c9d859d7fb
Adrian Martinez <adrian.martinez@bootcamp-upgrade.com>
2025-03-01 20:12:08 +0100

    Revert "Creacion de los getters y setters de las subclases"

--- src/Transporte/ConMotor.java
+++ src/Transporte/ConMotor.java
@@ -1,9 +1,3 @@
-package Transporte;
-
 public interface ConMotor {
-    
-    default void encenderMotor(){
-        System.out.println("El motor esta encendido.");
-    }
-}  
-
+    void encenderMotor();
+}
