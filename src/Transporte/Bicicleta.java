51d173b13ad8b6ce8e2d1508a7eb1d717d1bc7b9
Adrian Martinez <adrian.martinez@bootcamp-upgrade.com>
2025-03-01 22:36:57 +0100

    Actualizacion rama preMain
    Falta implementar las modificaciones el Main.java que hara el miembro Sergio.

--- src/Transporte/Bicicleta.java
+++ src/Transporte/Bicicleta.java
@@ -1,3 +1,22 @@
-public class Bicicleta {
+package Transporte;
 
-}
+public class Bicicleta extends Vehiculo implements SinMotor {
+
+    public Bicicleta(int velocidadMaxima, String marca, String modelo){
+        super(velocidadMaxima, marca, modelo);
+    }
+
+   
+    @Override
+    public void describir() {
+        System.out.println(
+            "Esta es una bicicleta marca "+marca+
+            " y modelo "+modelo+
+            ". Posee una velocidad máxima es " + velocidadMaxima + "km/h.");
+    }
+
+    @Override
+    public void usarFuerzaHumana() {
+        System.out.println("Usando fuerza humana para mover la bicicleta");
+    }
+    }
--- src/app/Main.java
+++ src/app/Main.java
@@ -1,5 +1,34 @@
+package app;
+
+import Transporte.Coche; 
+import Transporte.Bicicleta;
+import Transporte.Barco;
+import Transporte.Avion;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println();
+
+    public static void main (String[] args){
+
+        Coche coche1 = new Coche(195, "Toyota", "AE86");
+        coche1.arrancar();
+        coche1.encenderMotor();
+        coche1.describir();
+
+        Bicicleta bicicleta1 = new Bicicleta(80, "Orbea", "OIZ M-LTD");
+        bicicleta1.arrancar();
+        bicicleta1.usarFuerzaHumana();
+        bicicleta1.describir();
+
+        Barco barco1 = new Barco(58, "Incat", "045");
+        barco1.arrancar();
+        barco1.encenderMotor();
+        barco1.describir();
+
+        Avion avion1 = new Avion(492, "Airbus", "Beluga XL");
+        avion1.arrancar();
+        avion1.encenderMotor();
+        avion1.describir();
+
+        System.out.println(avion1.getMarca());
     }
 }
