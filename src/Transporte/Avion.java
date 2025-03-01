fabe03972d6c8e4d9e8ff3c3f249cdf046c69e28
Adrian Martinez <adrian.martinez@bootcamp-upgrade.com>
2025-03-01 21:08:48 +0100

    delete getters y setters

--- src/Transporte/Avion.java
+++ src/Transporte/Avion.java
@@ -6,34 +6,6 @@ public final class Avion extends Vehiculo implements ConMotor {
         super(velocidadMaxima, marca, modelo);
     }
 
-    public int getVelocidadMaxima(){
-        return velocidadMaxima;
-    }
-
-    public String getMarca(){
-        return marca;
-    }
-
-    public String getModelo(){
-        return modelo;
-    }
-
-    public void setVelocidadMaxima(int velocidadMaxima){
-        if(velocidadMaxima >= 0){ 
-            this.velocidadMaxima = velocidadMaxima;
-        } else {
-            System.out.println("La velocidad no puede ser negativa.");
-        } 
-    }
-
-    public void setMarca(String marca){
-        this.marca = marca;
-    }
-
-    public void setModelo(String modelo){
-        this.modelo = modelo;
-    }
-
     public void describir(){
         System.out.println("El avion "+marca+" "+modelo+" alcanza una velocidad maxima de "+velocidadMaxima+" nudos.");
     }
