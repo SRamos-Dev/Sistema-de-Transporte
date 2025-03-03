93a996514ead52272441f612ca4028ad656ed3c9
Franck <francklopez92@outlook.es>
2025-03-03 19:45:19 +0100

    Update
    Done

--- src/Transporte/ConMotor.java
+++ src/Transporte/ConMotor.java
@@ -1,8 +1,8 @@
 package Transporte;
 
 public interface ConMotor {
-    
-    default void encenderMotor(){
+
+    default void encenderMotor() {
         System.out.println("El motor esta encendido.");
     }
-}  
+}
